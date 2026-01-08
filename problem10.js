666bcd73e422b8b0d3b3ed7c9de185165c6c0e01
Azizul Haque <online.edu42@gmail.com>
2026-01-08 14:12:04 +0600

    solve problem 10

---
+++ problem10.js
@@ -0,0 +1,14 @@
+function pingPong() {
+  const n = 20;
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
